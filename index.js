feba3561f1bf117912dcf0725ede3befdc3b2e66
AllenLawrenceBolanos <allen.lawrence.bolanos@codeandtheory.com>
2022-02-09 13:04:22 +0800

    Added add operation

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log("Hello!")
+function add() {
+    const sum = parseInt(document.getElementById("input1").value) + parseInt(document.getElementById("input2").value);
+    const value = "Sum: " + sum;
+    document.getElementById("output").innerText = value;
+}
